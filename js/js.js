89429dbbafe871414362662cf08be2776ddd6d4d
eshban14 <eshban.younis@gmail.com>
2024-07-19 10:16:54 +0400

    first commit

---
+++ js/js.js
@@ -0,0 +1,25 @@
+
+
+
+// function changecolorred(){
+//     document.body.style.background = "red"
+// }
+
+// function changecolorblue(){
+//     document.body.style.background = "blue"
+// }
+// function changecolorgreen(){
+//     document.body.style.background = "green"
+// }
+// function changecolordefault(){
+//     document.body.style.background = "white"
+// }
+
+function chcolor(color){
+    document.body.style.background = color
+}
+
+function student(lastname, firstname, age = "17", Status = "Active"){
+    console.log(`my name is ${firstname} ${lastname}. I am ${age} years old. My study is ${Status}`) 
+}
+
